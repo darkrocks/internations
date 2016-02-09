22003b2dab7a82d012150f35ba854d9c39d68987
kazinov <kazinov.dima@gmail.com>
2016-02-09 14:22:05 +0700

    change data models

--- src/client/app/data/index.js
+++ src/client/app/data/index.js
@@ -4,56 +4,67 @@ import cloneDeep from 'lodash/cloneDeep'
 let groups = [
   {
     id: 1,
-    name: 'group 1',
-    users: [
-      1,
-      2
-    ]
+    name: 'group 1'
   },
   {
     id: 2,
-    name: 'group 2',
-    users: [
-      1,
-      2
-    ]
+    name: 'group 2'
   }
 ];
 
 let users = [
   {
     id: 1,
-    name: 'john'
+    name: 'john',
+    groups: [
+      1, 2
+    ]
   },
   {
     id: 2,
-    name: 'mary'
+    name: 'mary',
+    groups: [
+      1, 2
+    ]
   }
 ]
 
 export function getGroups() {
   return new Promise((resolve, reject) => {
-    resolve(groups);
+    var groupModels = [];
+
+    groups.forEach((g) => {
+      groupModels.push(groupToGroupDetails(g));
+    });
+
+    resolve(groupModels);
   })
+}
+
+function groupToGroupDetails(group) {
+  var groupModel = cloneDeep(group);
+  groupModel.users = [];
+
+  users.forEach((u) => {
+    var isUserInGroup = !!find(u.groups, (userGroupId) => userGroupId === group.id );
+
+    if (isUserInGroup) {
+      var userModel = cloneDeep(u);
+      delete userModel.groups;
+
+      groupModel.users.push(userModel);
+    }
+  });
+
+  return groupModel;
 }
 
 export function getGroupDetails(groupId) {
   groupId = parseInt(groupId)
 
   return new Promise((resolve, reject) => {
-
-    var group = cloneDeep(find(groups, (g) => g.id === groupId));
-    if (group) {
-      group.users = group.users.map((userId) => {
-        var user = find(users, (u) => u.id === userId);
-        if (user) {
-          return user;
-        }
-        return null;
-      });
-    }
-
-    resolve(group);
+    var group = find(groups, (g) => g.id === groupId);
+    resolve(groupToGroupDetails(group));
   })
 }
 
@@ -67,20 +78,25 @@ export function getUserDetails(userId) {
   userId = parseInt(userId)
 
   return new Promise((resolve, reject) => {
-    var user = cloneDeep(find(users, (u) => u.id === userId));
+    var userModel;
+    var user = find(users, (u) => u.id === userId);
 
     if (user) {
-      user.groups = [];
-      groups.forEach((g) => {
-        if (find(g.users, (groupUserId) => groupUserId === userId)) {
-          var groupToAdd = cloneDeep(g);
-          delete groupToAdd.users;
-          user.groups.push(groupToAdd);
+      userModel = cloneDeep(user);
+      const userGroupsIds = userModel.groups;
+      userModel.groups = [];
+      userGroupsIds.forEach((userGroupId) => {
+        var group = find(groups, (group) => group.id === userGroupId);
+
+        if (group) {
+          let groupModel = cloneDeep(group);
+          delete groupModel.users;
+          userModel.groups.push(groupModel);
         }
       });
     }
 
-    resolve(user);
+    resolve(userModel);
   })
 }
 
@@ -91,12 +107,9 @@ export function insertUser (user) {
       name: user.name
     };
 
-    users.push(userToAdd);
+    userToAdd.groups = user.groups.map((g) => g.id);
 
-    user.groups.forEach((userGroup) => {
-      var groupToAmmend = find(groups, (g) => g.id === userGroup.id);
-      groupToAmmend.users.push(userToAdd.id);
-    });
+    users.push(userToAdd);
     resolve(userToAdd);
   })
 }
