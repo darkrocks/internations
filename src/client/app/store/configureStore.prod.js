d8c4f58ca7c0ca54c729f0b8507a47081575050a
kazinov <kazinov.dima@gmail.com>
2016-02-11 15:35:53 +0700

    clean up

--- src/client/app/store/configureStore.prod.js
+++ src/client/app/store/configureStore.prod.js
@@ -2,13 +2,12 @@ import { createStore, applyMiddleware } from 'redux'
 import { syncHistory } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 import thunk from 'redux-thunk'
-import api from '../middleware/api'
 import rootReducer from '../reducers'
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, api, syncHistory(browserHistory))
+    applyMiddleware(thunk, syncHistory(browserHistory))
   )
 }
